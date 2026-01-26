a8146e18d5d3638f994b939d683947d8790f544d
burak-adanir <oebada23@gmail.com>
2026-01-26 11:12:14 +0100

    Phase 2: Die Infrastruktur (Terraform)

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_lb" "app_alb" {
+  name               = "p4-load-balancer"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+}
+
+resource "aws_lb_target_group" "blue" {
+  name = "tg-blue"
+  port = 8080
+  protocol = "HTTP"
+  vpc_id = aws_vpc.main.id
+  health_check {
+    path = "/api/employees"
+  }
+}
+
+resource "aws_lb_target_group" "green" {
+  name = "tg-green"
+  port = 8080
+  protocol = "HTTP"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = 100
+      }
+      target_group {
+        arn    = aws_lb_target_group.green.arn
+        weight = 0
+      }
+    }
+  }
+}
