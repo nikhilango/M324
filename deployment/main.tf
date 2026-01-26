7ee0c59c170f5282b935c52a6ccb9ea7988e0479
burak-adanir <oebada23@gmail.com>
2026-01-26 11:39:16 +0100

    Add Internet Gateway and Routing for internet access

--- deployment/main.tf
+++ deployment/main.tf
@@ -18,6 +18,30 @@ resource "aws_subnet" "sub2" {
   availability_zone = "us-east-1b"
 }
 
+# Internet-Gateway (Der Router nach draußen)
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route Table (Sagt dem Server: Geh über das Gateway ins Internet)
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.rt.id
+}
+
 resource "aws_lb" "app_alb" {
   name               = "p4-load-balancer"
   load_balancer_type = "application"
